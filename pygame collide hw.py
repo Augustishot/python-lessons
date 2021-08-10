d8d666a6806974155696864abee49e59d3c1bdce
Augustishot <86942110+Augustishot@users.noreply.github.com>
2021-08-10 18:38:20 +0800

    Add files via upload

--- pygame collide hw.py
+++ pygame collide hw.py
@@ -20,7 +20,11 @@ c = random.randint(10, 790)
 
 gh_y = 0
 
+score = 0
+get_score = False
+
 text_font = pygame.font.Font('INVASION2000.TTF', 50)
+text_font2 = pygame.font.Font('INVASION2000.TTF', 25)
 
 bg_surf = pygame.image.load('Background.jpeg').convert_alpha()
 gh_surf = pygame.image.load('ghost.png').convert_alpha()
@@ -32,17 +36,55 @@ ex_surf = pygame.image.load('explosion.png').convert_alpha()
 text_surf = text_font.render("When they collide...", False, 'Black')
 text2_surf = text_font.render("Boom!", False, 'Red')
 
+pos = pygame.mouse.get_pos()[0]
+pos2 = pygame.mouse.get_pos()[0]
+
+n = 2
+n2 = 1
+speed = False
+
+def move(n, pos, pos2):
+    if pos < pos2:
+        va_rect.left = va_rect.left - n
+    if pos > pos2:
+        va_rect.right = va_rect.right + n
+
+def su(n):
+    n = n + 0.05
+    return n
+
+def ss():
+    n = 2
+    return n
+
 while True:
+    pos2 = pos
+    tse = pygame.time.get_ticks()
+    tse = int(tse / 1000)
+    pos = pygame.mouse.get_pos()[0]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Score :', str(score))
+            print('Time :', str(tse))
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEMOTION:
-            if va_rect.right < pygame.mouse.get_pos()[0]:
-                va_rect.right = va_rect.right + 2
-            if va_rect.left > pygame.mouse.get_pos()[0]:
-                va_rect.left = va_rect.left - 2
-            
+            if pygame.mouse.get_pressed()[0]:
+                n = su(n)
+                move(n, pos, pos2)
+            else:
+                n = ss()
+                move(n, pos, pos2)   
+    
+    text3_surf = text_font2.render("score : " + str(score), False, 'Violet')
+    text4_surf = text_font2.render("time : " + str(tse) + " seconds", False, 'Violet')
+    
+    if n2 == 1:
+        pos2 = pygame.mouse.get_pos()[0] + 0.01
+        n2 = n2 + 1
+    else:
+        pos2 = pygame.mouse.get_pos()[0]
+    
     screen.blit(bg_surf, (0, 0))
            
     gh_rect.bottom = gh_rect.bottom + 2
@@ -61,9 +103,19 @@ while True:
         screen.blit(text2_surf, (300, 100))
     else:
         screen.blit(text_surf, (150, 100))
+    
+    if gh_rect.colliderect(va_rect) and get_score == False:
+        old_score = score
+        score = score + 1
+        get_score = True
+        
+    if get_score == True and not gh_rect.colliderect(va_rect):
+        get_score = False
 
     screen.blit(gh_surf, (gh_rect))
     screen.blit(va_surf, (va_rect))
+    screen.blit(text3_surf, (0, 0))
+    screen.blit(text4_surf, (0, 20))
 
     pygame.display.update()
     clock.tick(100)
